ddd54bb97af6dcf733df6021bd93cf3e8dad6f55
wjhdewinter <wjhdewinter@gmail.com>
2026-04-26 23:39:21 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="final-countdown-pro-v6";
-const ASSETS=["./","./index.html","./style.css?v=6","./app.js?v=6","./manifest.webmanifest?v=6","./icons/icon-192.png?v=6","./icons/icon-512.png?v=6","./icons/maskable-192.png?v=6","./icons/maskable-512.png?v=6"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
